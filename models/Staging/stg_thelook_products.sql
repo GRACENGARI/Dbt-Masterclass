1e9a1c3a890e3de5061bb7b060bd713cb4372c1c
GRACE NGARI <neemangari0@gmail.com>
2026-08-23 20:55:25 +0000

    updated  sources,materialization and seeds

--- models/Staging/stg_thelook_products.sql
+++
@@ -1,2 +0,0 @@
--- stg_thelook_products.sql
-select * from {{ source('thelook_ecommerce', 'products') }}
--- models/Staging/stg_thelook_users.sql
+++
@@ -1,2 +0,0 @@
--- stg_thelook_users.sql
-select * from {{ source('thelook_ecommerce', 'users') }}
